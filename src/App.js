dc55253a47c17ab293f69ba0dda6e6c0f127c66f
Abhinav <abhisri1997@gmail.com>
2024-12-25 10:39:13 +0530

    remove links for each users

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GuestProfile from './GuestProfile';
 import './App.css';
-import { guests } from './app.constant';
 
 
 const App = () => {
   return (
     <div className="container">
       <Router>
-        <nav className="guest-nav">
-          <div className="guest-links">
-            {guests.map(guest => (
-              <Link
-                key={guest.id}
-                to={`/guest/${guest.name.toLowerCase()}`}
-                className="guest-link"
-              >
-                {guest.name}
-              </Link>
-            ))}
-          </div>
-        </nav>
-
         <Routes>
           <Route path="/:name" element={<GuestProfile />} />
-          <Route path="/" element={<h2>Select a guest to view their QR code</h2>} />
         </Routes>
       </Router>
     </div>
