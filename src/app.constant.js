60168ca6f58d30c76d76a7df745a8fb55d6f91bc
Abhinav <abhisri1997@gmail.com>
2024-12-25 11:03:06 +0530

    add event rate the waste

--- src/app.constant.js
+++ src/app.constant.js
@@ -1,8 +1,13 @@
 export const events = [
     {
+        id: 0,
+        name: "Rate the waste",
+        description: "Each participant will bring their own booze and then we will make a cocktail out of it.",
+    },
+    {
         id: 1,
         name: "What's in my mouth?",
-        description: "A pair will be blindfolded, and one volunteer will help them eat different food items.",
+        description: "A pair will be blindfolded, and one volunteer will help them eat different food items. If they guess it wrong they have to drink.",
     },
     {
         id: 2,
@@ -22,11 +27,11 @@ export const events = [
 ];
 
 export const guests = [
-    { id: 1, name: 'Vaishnavi', role: 'participant', personality: 'Enthusiastic Foodie' },
-    { id: 2, name: 'Aseem', role: 'participant', personality: 'Dad Joke Expert' },
-    { id: 3, name: 'Priyansh', role: 'host', personality: 'Game Master' },
-    { id: 4, name: 'Vikas', role: 'participant', personality: 'Quick Guesser' },
-    { id: 5, name: 'Arjun', role: 'participant', personality: 'Food Taster' },
-    { id: 6, name: 'Aarti', role: 'participant', personality: 'Creative Mind' },
-    { id: 7, name: 'Prabhat', role: 'participant', personality: 'Strategic Player' }
+    { id: 1, name: 'Vaishnavi', role: 'Participant', personality: 'Enthusiastic Foodie' },
+    { id: 2, name: 'Aseem', role: 'Participant', personality: 'Dad Joke Expert' },
+    { id: 3, name: 'Priyansh', role: 'Participant', personality: 'Game Master' },
+    { id: 4, name: 'Vikas', role: 'Participant', personality: 'Quick Guesser' },
+    { id: 5, name: 'Arjun', role: 'Participant', personality: 'Food Taster' },
+    { id: 6, name: 'Aarti', role: 'Participant', personality: 'Creative Mind' },
+    { id: 7, name: 'Prabhat', role: 'Bouncer', personality: 'Strategic Player' }
 ];
